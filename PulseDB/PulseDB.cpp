b612f02537017a6fbc43f82593e34a7bd8b4dae4
Fionn Hughes <fionn.hughes@ucdconnect.ie>
2026-03-29 13:55:36 +0100

    chore: initial project scaffolding

--- PulseDB/PulseDB.cpp
+++ PulseDB/PulseDB.cpp
@@ -1,9 +1,14 @@
 ﻿#include "PulseDB.h"
+#include "storage/PulseFileWriter.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	pulsedb::PulseFileWriter writer("test.pulse", MetricType::cpu_total, "cpu_total");
+	writer.open();
+
 	return 0;
 }
