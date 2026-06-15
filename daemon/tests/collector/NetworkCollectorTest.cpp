db2ce6a6e4603ba5f1012bee9fe97c90145b9985
Fionn Hughes <fionn.hughes1@gmail.com>
2026-06-15 02:26:17 +0100

    test(collector): add NetworkCollectorTest

---
+++ daemon/tests/collector/NetworkCollectorTest.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+
+#include "collector/NetworkCollector.h"
+#include "collector/MetricSnapshot.h"
+
+namespace pulsedb {
+
+    TEST(NetworkCollectorTest, InitializeSucceeds) {
+        NetworkCollector network_collector;
+        EXPECT_TRUE(network_collector.initialize());
+    }
+
+    TEST(NetworkCollectorTest, CollectAndFillSnapshots) {
+        NetworkCollector network_collector;
+        MetricSnapshot snap;
+
+        network_collector.initialize();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        network_collector.collect();
+        network_collector.fill_snapshot(snap);
+
+        EXPECT_GT(snap.network_adapters.size(), 0u);
+        for (const auto& a : snap.network_adapters) {
+            EXPECT_FALSE(a.adapter_name.empty());
+        }
+    }
+}
